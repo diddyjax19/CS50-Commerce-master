0468d5183b12b48b7c70e28058a2da8dc880a717
diddyjax19 <adetobi59@gmail.com>
2023-02-26 08:37:13 +0000

    changes made on view.py

--- auctions/views.py
+++ auctions/views.py
@@ -1,3 +1,12 @@
+# All relevant imports
+# References: < Havrard CS50 and Zach Barlow> 
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-
-# Create your views here.
+from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
+from django import forms
+# Handles client side Error exceptions
+from django.core.exceptions import ObjectDoesNotExist
+from django.db import IntegrityError
